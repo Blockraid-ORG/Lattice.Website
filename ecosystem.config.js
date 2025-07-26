275b49d85b2a3afe2c3e8e90b3db76a18ddf60be
uutbudiarto2012 <hikids.developer@gmail.com>
2025-07-26 17:30:48 +0700

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'alpha.terravest.capital.5000',
+      script: 'npm',
+      args: 'start',
+      env: {
+        PORT: 5000,
+      },
+    },
+  ],
+};
