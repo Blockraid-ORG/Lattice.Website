05096c5c89b2f9a00fae8e15bc323ed86aa54ff6
wahyunf354 <wahyunurfadillah313@gmail.com>
2025-08-04 19:56:34 +0700

    Enhance verification process with zkMe integration and update environment variables. Added zkMe widget for KYC verification in AlertVerification component, updated profile content to include wallet address, and improved verification service with new approval method. Also, added new environment variables for zkMe configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.alias['@react-native-async-storage/async-storage'] = false
+      config.resolve.alias["@react-native-async-storage/async-storage"] = false;
     }
-    return config
+    return config;
   },
   env: {
     PINATA_JWT: process.env.PINATA_JWT,
@@ -12,50 +12,54 @@ const nextConfig = {
     W3AUTH_CLIENT_ID: process.env.W3AUTH_CLIENT_ID,
     BASE_URL: process.env.BASE_URL,
     BLOCKSCOUT_API: process.env.BLOCKSCOUT_API,
+    ZKME_API_KEY: process.env.ZKME_API_KEY,
+    ZKME_APP_ID: process.env.ZKME_APP_ID,
+    ZKME_PROGRAM_NO: process.env.ZKME_PROGRAM_NO,
+    ZKME_DAPP_NAME: process.env.ZKME_DAPP_NAME,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'maroon-delicate-coyote-528.mypinata.cloud',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "maroon-delicate-coyote-528.mypinata.cloud",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'red-careful-koala-550.mypinata.cloud',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "red-careful-koala-550.mypinata.cloud",
+        port: "",
+        pathname: "/**",
       },
       {
-        hostname: 'copper-random-mammal-506.mypinata.cloud',
-        protocol: 'https',
+        hostname: "copper-random-mammal-506.mypinata.cloud",
+        protocol: "https",
       },
       {
-        protocol: 'https',
-        hostname: 'red-careful-koala-550.mypinata.cloud',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "red-careful-koala-550.mypinata.cloud",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'api-dev.e-wow.my.id',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "api-dev.e-wow.my.id",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'api.terravest.capital',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "api.terravest.capital",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**",
       },
-    ]
+    ],
   },
 };
 
--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -1,35 +1,42 @@
 "use client";
 
-import { Web3AuthProvider, type Web3AuthContextConfig } from "@web3auth/modal/react";
+import {
+  Web3AuthProvider,
+  type Web3AuthContextConfig,
+} from "@web3auth/modal/react";
 import { IWeb3AuthState, WEB3AUTH_NETWORK } from "@web3auth/modal";
 
 import { WagmiProvider } from "@web3auth/modal/react/wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
-const clientId = process.env.W3AUTH_CLIENT_ID ?? ""
+const clientId = process.env.W3AUTH_CLIENT_ID ?? "";
 
 const queryClient = new QueryClient();
 const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
     clientId,
-    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
-    // web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+    // web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
     ssr: true,
-  }
+  },
 };
 
-export default function ReactQueryProvider({ children, web3authInitialState }:
-  { children: React.ReactNode, web3authInitialState: IWeb3AuthState | undefined }) {
+export default function ReactQueryProvider({
+  children,
+  web3authInitialState,
+}: {
+  children: React.ReactNode;
+  web3authInitialState: IWeb3AuthState | undefined;
+}) {
   return (
-
-    <Web3AuthProvider config={web3AuthContextConfig} initialState={web3authInitialState}>
+    <Web3AuthProvider
+      config={web3AuthContextConfig}
+      initialState={web3authInitialState}
+    >
       <QueryClientProvider client={queryClient}>
-        <WagmiProvider>
-          {children}
-        </WagmiProvider>
+        <WagmiProvider>{children}</WagmiProvider>
       </QueryClientProvider>
     </Web3AuthProvider>
-
   );
 }
